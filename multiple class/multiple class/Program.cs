23ac8dfc618c9b00fdf7915a3d0b5979be8b0a45
Shuvo Debnath <50118392+shuvo84@users.noreply.github.com>
2026-01-16 04:54:31 +0600

    Add files via upload

---
+++ multiple class/multiple class/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car;
+            car = new Car("Red");
+            Console.WriteLine(car.Describe());
+            car.Color = "Green";
+            Console.WriteLine(car.Describe());
+
+            Console.ReadLine();
+        }
+    }
+    class Car
+    {
+        private string color;
+
+        public Car(string color)
+        {
+            this.color = color;
+        }
+
+        public string Describe()
+        {
+            return "This car is " + Color;
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+    }
+    class Calculator
+    {
+       
+    }
+}
